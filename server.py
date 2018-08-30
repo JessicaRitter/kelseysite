880ee4f845c6aee9860830771c6504b78807b616
Jess Ritter <jessicalynn.w.ritter@gmail.com>
2018-08-29 19:00:59 -0700

    getting prepped to push to heroku

--- server.py
+++ server.py
@@ -6,8 +6,6 @@ from flask import Flask, jsonify, render_template, redirect, request, flash, ses
 
 app = Flask(__name__)
 
-# Required to use Flask sessions and the debug toolbar
-app.secret_key = "ABC"
 
 # Normally, if you use an undefined variable in Jinja2, it fails
 # silently. This is horrible. Fix this so that, instead, it raises an
@@ -37,11 +35,12 @@ def show_home():
 if __name__ == "__main__":
     # We have to set debug=True here, since it has to be True at the
     # point that we invoke the DebugToolbarExtension
-    app.debug = True
-    app.jinja_env.auto_reload = app.debug  # make sure templates, etc. are not cached in debug mode
+    # app.debug = True
+    # app.jinja_env.auto_reload = app.debug  # make sure templates, etc. are not cached in debug mode
 
     # connect_to_db(app)
 
     # Use the DebugToolbar
-    DebugToolbarExtension(app)
-    app.run(host='0.0.0.0', port=5000)
+    PORT = int(os.environ.get("PORT", 5000))
+
+    app.run(host="0.0.0.0", port=PORT)
